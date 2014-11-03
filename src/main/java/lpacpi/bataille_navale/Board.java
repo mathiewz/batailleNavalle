9bf5ec24a3f3b5f8b18a8ceb4c96f858b0bcd19e
Steven Daniel <sdaniel@info-tyrionx.iut.bx1>
2014-11-03 17:52:02 +0100

    ajout du board

---
+++ src/main/java/lpacpi/bataille_navale/Board.java
@@ -0,0 +1,77 @@
+package lpacpi.bataille_navale;
+
+public class Board {
+	public static int DIMENSION=10;
+	private int[][] plateau = new int[DIMENSION][DIMENSION];
+	public static int SENS_HORIZONTAL=1;
+	public static int SENS_VERTICAL=-1;
+	public static int CASE_EAU=1;
+	public static int CASE_BATEAU=2;
+	public static int CASE_DANS_EAU=3;
+	public static int CASE_TOUCHE=4;
+
+	public void Board()	{
+		for(int i=0; i<DIMENSION;i++){
+			for(int j=0; i<DIMENSION;j++){
+				plateau[i][j] = CASE_EAU;
+			}
+		}
+	}
+	public void placerBateau(int coordonneeX,int coordonneeY,Bateau bateau,int sens)
+	{
+		if(sens==SENS_HORIZONTAL){
+			if(coordonneeX+bateau.GetTaille()<DIMENSION) {
+				boolean onPeutPlacer = true;
+				for(int n = 0; n < bateau.GetTaille(); n++){
+					if(plateau[coordonneeX+n][coordonneeY]==CASE_BATEAU){
+						onPeutPlacer = false;
+					}
+				}
+				if(onPeutPlacer){
+					for(int n = 0; n < bateau.GetTaille(); n++){
+						plateau[coordonneeX+n][coordonneeY]=CASE_BATEAU;
+					}
+				} else {
+					throw new Exception("Erreur : Case déja occupée");
+				}
+			} else {
+				throw new Exception ("Erreur : Le bateau dépasse du plateau !");
+			}
+
+		}
+
+		if(sens==SENS_VERTICAL){
+			if(coordonneeY+bateau.GetTaille()<DIMENSION) {
+				boolean onPeutPlacer = true;
+				for(int n = 0; n < bateau.GetTaille(); n++){
+					if(plateau[coordonneeX][coordonneeY+n]==CASE_BATEAU){
+						onPeutPlacer = false;
+					}
+				}
+				if(onPeutPlacer){
+					for(int n = 0; n < bateau.GetTaille(); n++){
+						plateau[coordonneeX][coordonneeY+n]=CASE_BATEAU;
+					}
+				} else {
+					throw new Exception("Erreur : Case déja occupée");
+				}
+			} else {
+				throw new Exception ("Erreur : Le bateau dépasse du plateau !");
+			}
+		}
+	}
+	
+	public String toString(){
+		String ret = new String();
+		ret += "Etat du board\n";
+		for(int i=0; i<DIMENSION;i++){
+			ret += " ------------------- ";
+			for(int j=0; i<DIMENSION;j++){
+				ret+= plateau[i][j]+"|";
+			}
+			ret += "\n";
+		}
+		ret += " ------------------- ";
+		return ret;
+	}
+}
