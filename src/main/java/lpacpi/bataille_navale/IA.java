5a87e26d623c0b237fc6ec7f056fa73a3328ba3d
Steven Daniel <steven.daniel@outlook.fr>
2014-11-07 13:32:01 +0100

    partie 21

--- src/main/java/lpacpi/bataille_navale/IA.java
+++ src/main/java/lpacpi/bataille_navale/IA.java
@@ -8,6 +8,20 @@ import java.util.Scanner;
 public class IA extends Board{
 
 	public IA(){
+		plateau = new int[DIMENSION][DIMENSION];
+		for(int i=0; i<DIMENSION;i++){
+			for(int j=0; j<DIMENSION;j++){
+				plateau[i][j] = CASE_EAU;
+			}
+		}
+		listBateaux = new ArrayList<Bateau>();
+		this.initialiserBateaux();
+	}
+
+
+	public IA(int size) {
+		DIMENSION = size;
+		plateau = new int[DIMENSION][DIMENSION];
 		for(int i=0; i<DIMENSION;i++){
 			for(int j=0; j<DIMENSION;j++){
 				plateau[i][j] = CASE_EAU;
---
+++ src/main/java/lpacpi/bataille_navale/Partie21.java
@@ -0,0 +1,27 @@
+package lpacpi.bataille_navale;
+
+import java.util.Scanner;
+
+public class Partie21 extends Partie {
+
+	public Partie21(){
+		System.out.println("Saisir votre nom de joueur");
+		Scanner sc = new Scanner(System.in);
+		String nomJoueur1=sc.nextLine();
+		System.out.println("Saisir le nom de l'adversaire");
+		Scanner sc1 = new Scanner(System.in);
+		String nomJoueur2=sc1.nextLine();
+		bj1 = new Board(21, nomJoueur1);
+		bj2 = new IA(21);
+		while(!bj2.isBoardGameOver()){
+			System.out.println("\nA "+nomJoueur1+ " de jouer !!\n================\n\n");
+			tour(bj1, bj2,true);
+		}
+		if(bj2.isBoardGameOver()){
+			System.out.println("Partie terminée !!!"+nomJoueur1+" à gagné !\n");
+		}else{
+			System.out.println("Partie terminée !!!"+nomJoueur2+" à gagné !\n");
+		}
+	}
+
+}
